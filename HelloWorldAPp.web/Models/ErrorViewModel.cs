718b306eb95ba0e415e5986834192761cc012996
nbgithubaccount <githubaccount@nexgbits.com>
2022-09-29 10:26:40 -0400

    First commit

---
+++ HelloWorldAPp.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWorldAPp.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
